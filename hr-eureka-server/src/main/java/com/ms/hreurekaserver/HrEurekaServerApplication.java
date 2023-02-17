648df68e2b01503be2b885fe7ed563d2ee226b8f
Laura Nunes <lauranunes2508@gmail.com>
2023-02-17 11:57:28 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/com/ms/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/ms/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,11 @@ package com.ms.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
